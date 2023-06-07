f3d08d196e0d00f66e52011a257556b963d4555e
Bilaal <73635058+bilaalukis@users.noreply.github.com>
2023-06-07 12:47:50 +0000

    add, delete and show

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,35 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import ReminderList from "./components/ReminderList";
 import Reminder from "./models/Reminder";
+import service from "./services/reminder";
+import NewReminder from "./components/NewReminder";
 
 function App() {
-  const [reminders, setReminders] = useState<Reminder[]>([
-    { id: 1, title: "Reminder 1" },
-  ]);
+  const [reminders, setReminders] = useState<Reminder[]>([]);
+
+  useEffect(() => {
+    loadReminders();
+  }, []);
+
+  const loadReminders = async () => {
+    const reminders = await service.getReminder();
+    setReminders(reminders);
+  };
+
+  const removeReminder = (id: number) => {
+    setReminders(reminders.filter((reminder) => reminder.id !== id));
+  };
+
+  const addReminder = async (title: string) => {
+    const newReminder = await service.addReminder(title);
+    setReminders([newReminder, ...reminders]);
+  };
 
   return (
     <div className="App">
-      <ReminderList items={reminders} />
+      <NewReminder onAddReminder={addReminder} />
+      <ReminderList items={reminders} onRemoveReminder={removeReminder} />
     </div>
   );
 }
